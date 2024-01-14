4a6d30cb34937b3ae250215f1edafca5ef1e3978
AshutoshWagh12 <waghashutosh12@gmail.com>
2024-01-14 18:28:12 +0530

    index.js updated

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import projectCardsSlice from "./projectCardsSlice";
 import certificationCardsSlice from "./certificationCardsSlice";
-import greetingSlice from "./GreetingSlice";
 import SocialMediaLinksSlice from "./SocialMediaLinksSlice";
 import skillsSlice from "./SkillsSlice";
 import contactPageSlice from "./contactPageSlice";
 import experienceCardsSlice from "./ExperienceCardsSlice";
+import greetingSlice from "./GreetingSlice";
 
 
 
